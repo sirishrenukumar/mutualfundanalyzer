8fdb002a96636e70ec630eab6c139222988b8e02
Sirish Renukumar <sirish.renukumar@gmail.com>
2014-07-13 23:51:08 +0530

    Added Jetty and Servlet dependency to pom,xml

--- src/main/java/com/sirishrenukumar/mfa/MutualFundAnalyzer.java
+++ src/main/java/com/sirishrenukumar/mfa/MutualFundAnalyzer.java
@@ -2,11 +2,14 @@ package com.sirishrenukumar.mfa;
 
 import java.io.IOException;
 
+import javax.annotation.ManagedBean;
 import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.sirishrenukumar.mfa.core.MutualFundPortfolioParser;
@@ -14,10 +17,18 @@ import com.sirishrenukumar.mfa.core.MutualFundSnapshotSummaryParser;
 import com.sirishrenukumar.mfa.entity.Stock;
 import com.sirishrenukumar.mfa.entity.managers.MutualFundAndStockManager;
 
-@Configuration
-@ComponentScan
-public class MutualFundAnalyzer {
+@ManagedBean
+public class MutualFundAnalyzer extends HttpServlet{
 	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		run();
+	}
+
 	@Inject
 	private MutualFundSnapshotSummaryParser mutualFundSnapshotSummaryParser;
 
